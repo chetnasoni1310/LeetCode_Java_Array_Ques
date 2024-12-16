bd57040060c9ae70b4ac8c4d8930fd1e7a829fde
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2024-12-16 17:07:49 +0530

    ok

---
+++ 3264_final_array_K_Operation.java
@@ -0,0 +1,17 @@
+class  3264_final_array_K_Operation{
+    public int[] getFinalState(int[] nums, int k, int multiplier) {
+        while(k-->0)
+        {
+        int min=0;
+        for(int i=0;i<nums.length;i++)
+        {
+             if(nums[min]>nums[i])
+             {
+                min=i;
+             }
+        }
+        nums[min]*=multiplier;
+        }
+        return nums;
+    }
+}
