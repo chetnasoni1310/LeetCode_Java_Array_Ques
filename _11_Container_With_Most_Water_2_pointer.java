fc04022f9d9cb6b4510909d249e5d89df3b3ff1b
Chetna Soni <143208831+chetnasoni1310@users.noreply.github.com>
2025-01-17 15:08:30 +0530

    updated

---
+++ _11_Container_With_Most_Water_2_pointer.java
@@ -0,0 +1,24 @@
+public class _11_Container_With_Most_Water_2_pointer {
+     // 11. Container With Most Water
+    // https://leetcode.com/problems/container-with-most-water/
+    public int maxArea(int[] arr) {
+        int n=arr.length;
+        int i=0;
+        int j=n-1;
+        int area=0;
+        while(i<j)
+        {
+            int currarea=Math.min(arr[i],arr[j]) *(j-i);
+            area=Math.max(area,currarea);
+            if(arr[i]<=arr[j])
+            {
+                  i++;
+            }
+            else
+            j--;
+            
+        }
+        return area;
+    }
+}
+
